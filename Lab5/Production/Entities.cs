589635b5ea75485c7e82e61d7754b8adb6aa2c02
Никита <48802025+eternalforgotten@users.noreply.github.com>
2021-12-07 00:14:56 +0300

    parsing facts

---
+++ Lab5/Production/Entities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Production
+{
+    enum FactLayer { INITIAL = 0, SECOND, THIRD, FINAL, UNDEFINED}
+    class Fact
+    {
+        public String description;
+        public int number;
+        public FactLayer layer;
+        public Fact(String description, int number, FactLayer layer)
+        {
+            this.description = description;
+            this.number = number;
+            this.layer = layer;
+        }
+    }
+}
---
+++ Lab5/Production/Parsing.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Production
+{
+    class Parsing
+    {
+        public static List<Fact> ParseFacts(String fileName)
+        {
+            List<Fact> facts = new List<Fact>();
+            if (File.Exists(fileName))
+            {
+                foreach (var line in File.ReadLines(fileName))
+                {
+                    var splitted = line.Split(';');
+                    var description = splitted.Last();
+                    var layerAndNumber = splitted.First();
+                    var layerString = layerAndNumber[0];
+                    var number = int.Parse(layerAndNumber.Substring(1));
+                    FactLayer factLayer;
+                    switch (layerString)
+                    {
+                        case 'A': factLayer = FactLayer.INITIAL; break;
+                        case 'B': factLayer = FactLayer.SECOND; break;
+                        case 'C': factLayer = FactLayer.THIRD; break;
+                        case 'D': factLayer = FactLayer.FINAL; break;
+                        default: factLayer = FactLayer.UNDEFINED; break;
+                    }
+                    Fact fact = new Fact(description, number, factLayer);
+                    facts.Add(fact);
+                }
+                return facts;
+            }
+            else
+            {
+                throw new Exception("No such file");
+            }
+        }
+    }
+}
